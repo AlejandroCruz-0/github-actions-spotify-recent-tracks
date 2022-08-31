a7242e6673cd52f8295bf01de914fa0fa05e0164
Alejandro Cruz <alejandro.cruz.jeria@gmail.com>
2022-08-31 17:36:32 -0400

    Cambie formato de lista de canciones

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import httpx
-from dotenv import load_dotenv, find_dotenv  #testing
+from dotenv import load_dotenv, find_dotenv  # testing
 import os
 from datetime import datetime
 import pytz
 import logging
 import logging.handlers
 
-#testing
+# testing
 load_dotenv(find_dotenv())
 
 logger = logging.getLogger(__name__)
@@ -27,7 +27,7 @@ CLIENT_ID = os.environ.get("CLIENT_ID").strip()
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET").strip()
 
 
-def get_refresh_token():    
+def get_refresh_token():
     data = {
         "grant_type": "refresh_token",
         "refresh_token": REFRESH_TOKEN,
@@ -66,7 +66,7 @@ def get_user_info(access_token):
 
 
 def get_recently_played_tracks(access_token):
-    url = 'https://api.spotify.com/v1/me/player/recently-played'
+    url = "https://api.spotify.com/v1/me/player/recently-played"
     headers = {
         "Authorization": "Bearer " + access_token,
     }
@@ -80,12 +80,13 @@ def get_recently_played_tracks(access_token):
         logger.error(error)
         exit()
 
+
 def get_chilean_time(time):
-    #Función para convertir el tiempo UTC sin offset de la propiedad played_at
-    #De la respuesta de la api de Spotify, a tiempo Chileno
+    # Función para convertir el tiempo UTC sin offset de la propiedad played_at
+    # De la respuesta de la api de Spotify, a tiempo Chileno
 
     try:
-        #Mejorar esto
+        # Mejorar esto
         year = int(time[:4])
         month = int(time[5:7])
         day = int(time[8:10])
@@ -93,45 +94,59 @@ def get_chilean_time(time):
         minute = int(time[14:16])
         second = int(time[17:19])
 
-        #Crear nueva datetime con tiempo utc
-        my_datetime = datetime(year, month, day, hour, minute, second, tzinfo = pytz.utc)
-        #convertir a tiempo chileno
-        my_chile_time = my_datetime.astimezone(pytz.timezone('America/Santiago')).strftime('%Y-%m-%d %H:%M:%S')
+        # Crear nueva datetime con tiempo utc
+        my_datetime = datetime(year, month, day, hour, minute, second, tzinfo=pytz.utc)
+        # convertir a tiempo chileno
+        my_chile_time = my_datetime.astimezone(
+            pytz.timezone("America/Santiago")
+        ).strftime("%Y-%m-%d %H:%M:%S")
         return my_chile_time
     except Exception as error:
         logger.error(error)
         exit
-            
 
 
 def main():
     token = get_refresh_token()
-    userinfo = get_user_info(token)     
-    tracks = get_recently_played_tracks(token) 
-    tracks_played = []
+    userinfo = get_user_info(token)
+    tracks = get_recently_played_tracks(token)
+    tracks_played = {}
 
-    
-    for track in tracks['items']:
+    for id, track in enumerate(tracks["items"]):
         track_name = f"**{track['track']['name']}**"
-        track_artists = [x['name']  for x in track['track']['artists']]
-        track_album = track['track']['album']['name']        
-        played_at = get_chilean_time(track['played_at'])
-        full_track_info = f'{track_name} - {"|".join(track_artists)} Album: {track_album} | {played_at}'
-        tracks_played.append(full_track_info)
+        track_artists = [x["name"] for x in track["track"]["artists"]]
+        track_album = track["track"]["album"]["name"]
+        played_at = get_chilean_time(track["played_at"])
+        album_image = track["track"]["album"]["images"][1]["url"]  # tamaño 300
+        tracks_played[id] = {
+            "track_name": track_name,
+            "track_artists": track_artists,
+            "track_album": track_album,
+            "played_at": played_at,
+            "album_image": album_image,
+        }
 
-    
-
-    with open("README.md","w") as file:
+    with open("README.md", "w") as file:
         file.write("# github-actions-spotify-recent-tracks\n")
-        file.write("muestro las ultimas canciones de mi cuenta spotify usando github actions\n")
+        file.write(
+            "muestro las ultimas canciones de mi cuenta spotify usando github actions\n"
+        )
         file.write("# Info de mi Cuenta\n")
         file.write(f"Nombre: **{userinfo['display_name']}**\n")
         file.write(f"[Link perfil spotify]({userinfo['external_urls']['spotify']})\n")
         file.write("")
         file.write("# Canciones:\n")
         file.write("\n")
-        for track in tracks_played:
-            file.write(f"- {track}\n")
+        for id, track in tracks_played.items():
+            file.write(
+                f"# Canción: **{track['track_name']}** - **{'|'.join(track['track_artists'])}**\n"
+            )
+            file.write(
+                f"![Foto del album: {track['track_album']}]({track['album_image']})\n"
+            )
+            file.write(f"Album: {track['track_album']}\n")
+            file.write(f"Tiempo de reproducción: {track['played_at']}\n")
+            file.write("_________________________________________________")
 
     logger.info("Operación completada")
 
